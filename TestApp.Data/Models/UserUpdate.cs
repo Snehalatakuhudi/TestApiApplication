31092910b11d8ce600bc48407fd2f7635e78ef82
Snehalatakuhudi <52893009+Snehalatakuhudi@users.noreply.github.com>
2019-09-01 13:42:28 +0530

    Update UserUpdate.cs

--- TestApp.Data/Models/UserUpdate.cs
+++ TestApp.Data/Models/UserUpdate.cs
@@ -11,8 +11,7 @@ namespace TestApp.Data.Models
         public int UserId { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
-        public int? Age { get; set; }
+        public string Password { get; set; } 
         public DateTime? DOB { get; set; }
         public string Location { get; set; }
         public string UsrType { get; set; }
